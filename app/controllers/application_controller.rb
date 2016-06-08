884ea32371b0ca98a91bbd6756de1ea150a14aba
Rylan Bowers <rylanb@gmail.com>
2016-06-08 17:53:51 -0600

    Whitespace cleanup in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,11 +18,11 @@ class ApplicationController < ActionController::Base
   ActiveScaffold.set_defaults do |config|
     config.security.current_user_method = :current_volunteer
   end
-  
+
   layout :layout_by_resource
 
   protected
-  
+
     def layout_by_resource
       if devise_controller?
         "custom_devise"
