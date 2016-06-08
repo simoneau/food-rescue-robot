9a325d731308dc089fc52152087834d4a969d9f4
Rylan Bowers <rylanb@gmail.com>
2016-06-08 17:54:04 -0600

    Adding secret token update for dev / test.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
   Webapp::Application.config.secret_token = 'dev_0aac6asdfalsdfjkalsdfkdlsfljkasljkdflkajsflkjsdlkjflksdfjdsklf'
 else
   Webapp::Application.config.secret_token = ENV['SECRET_KEY_BASE']
